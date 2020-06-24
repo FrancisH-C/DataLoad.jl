0f2eb8100e4f955754b96c138b9df69c38ef6d03
FrancisH-C <francishchantal@gmail.com>
2020-06-24 10:26:55 -0400

    removing displying it julia's template (too many output)

--- templates/template_julia.jl
+++ templates/template_julia.jl
@@ -5,6 +5,5 @@ stocks_list=<function_to_load>
 # this takes ≈ 3.23 hours on ssd
 @time for (stock, day) in stocks_list
     @time data = import_data(stock, day, data_type="orders", verbose=true)
-    display(data) # for testing purposes keep it, otherwise delete it
     error("Stop after one iteration, comment this to continue iterating.")
 end
