f18518444358ea35c19c703410d4c41de8e954ce
FrancisH-C <francishchantal@gmail.com>
2020-06-24 10:22:48 -0400

    display in julia's template

--- templates/template_julia.jl
+++ templates/template_julia.jl
@@ -4,6 +4,7 @@ stocks_list=<function_to_load>
 # this takes ≈ 3.39 hours on hdd
 # this takes ≈ 3.23 hours on ssd
 @time for (stock, day) in stocks_list
-        @time data = import_data(stock, day, data_type="orders", verbose=true)
-	error("Stop after one iteration, comment this to continue iterating.")
+    @time data = import_data(stock, day, data_type="orders", verbose=true)
+    display(data) # for testing purposes keep it, otherwise delete it
+    error("Stop after one iteration, comment this to continue iterating.")
 end
