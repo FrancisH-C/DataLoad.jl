d095421d3b77b08c292e18afcc22a34b39076ed6
FrancisH-C <francishchantal@gmail.com>
2020-06-30 11:51:13 -0400

    import_post_processed added

--- src/lib/import_post_processed.jl
+++ src/lib/import_post_processed.jl
@@ -1,5 +1,13 @@
-function import_log()
+function log_files(relative_path::String)
+    directory =  "$DATA_OUTPUT/$relative_path"
+    itr = walkdir(directory) 
+    (root, dirs, files) = first(itr)
+    (root, dirs, files) = first(itr)
+    return joinpath.(root, files) # path to files
 end
 
-function import_output_data()
+function output_data_files(relative_path::String)
+    directory =  "$DATA_OUTPUT/$relative_path"
+    (root, dirs, files) = first(walkdir(directory))
+    return joinpath.(root, files) # path to files
 end
