7702ba31ac885f437f681d885e8686e888e5b95b
FrancisH-C <francishchantal@gmail.com>
2020-07-01 12:58:21 -0400

    the case when exported is nothing (length = 0)

--- src/lib/export_data.jl
+++ src/lib/export_data.jl
@@ -2,7 +2,10 @@ function data_exporter(relative_path::String, to_df::Function=identity)
     directory = "$DATA_OUTPUT/$relative_path/"
     run(`mkdir -p $directory`)
     f = function export_data(stock::String, day::Int64, anything::Any)
-        export_pqt("$directory$day$(stock).pqt", to_df(anything))
+        df = to_df(anything)
+        if size(df, 1) != 0
+            export_pqt("$directory$day$(stock).pqt", df)
+        end
     end
     return f
 end
